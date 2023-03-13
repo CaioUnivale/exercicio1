11005ca6469e7041f10f672e3256e17b1fd1f04f
CaioUnivale <estudos151927@gmail.com>
2023-03-13 17:30:39 -0300

    first commit

---
+++ exercicio1.py
@@ -0,0 +1,12 @@
+nome = "Caio"
+sobrenome = "Silva"
+idade = 20
+altura = 1.68
+peso = 68
+maior_de_idade = idade >= 18
+
+print("Nome completo:", nome, sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Maior de idade?", maior_de_idade)
